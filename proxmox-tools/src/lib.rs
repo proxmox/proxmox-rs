d7fbee66e30f75ca131abc50b9feed6c6f163ced
Dietmar Maurer <dietmar@proxmox.com>
2019-06-14 09:55:10 +0200

    proxmox-tools/src/lib.rs: implement offsetof

--- proxmox-tools/src/lib.rs
+++ proxmox-tools/src/lib.rs
@@ -2,3 +2,11 @@
 
 pub mod io;
 pub mod vec;
+
+/// Evaluates to the offset (in bytes) of a given member within a struct
+#[macro_export]
+macro_rules! offsetof {
+    ($ty:ty, $field:ident) => {
+        unsafe { &(*(0 as *const $ty)).$field as *const _ as usize }
+    }
+}
