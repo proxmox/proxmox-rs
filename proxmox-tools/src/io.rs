e290d7884031c02d08810b7a450e342402bc340b
Wolfgang Bumiller <w.bumiller@proxmox.com>
2019-07-01 10:56:27 +0200

    remove deprecated tools::io::ops reexport
    Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>

--- proxmox-tools/src/io.rs
+++ proxmox-tools/src/io.rs
@@ -3,11 +3,6 @@
 //! The [`ReadExt`] trait provides additional operations for handling byte buffers for types
 //! implementing [`Read`](std::io::Read).
 
-// DEPRECATED:
-pub mod ops {
-    pub use super::ReadExt as ReadExtOps;
-}
-
 mod read;
 pub use read::*;
 
