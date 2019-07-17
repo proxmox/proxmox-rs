bfbba4e6054ae3a48cfda69dee99b43f2e95e4fd
Wolfgang Bumiller <w.bumiller@proxmox.com>
2019-07-17 16:13:36 +0200

    tests: use an async block instead of an async closure
    Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>

--- proxmox-api-macro/tests/basic.rs
+++ proxmox-api-macro/tests/basic.rs
@@ -75,9 +75,9 @@ async fn get_loopback(param: String) -> Result<String, Error> {
     returns: String
 })]
 fn non_async_test(param: String) -> proxmox::api::ApiFuture<Bytes> {
-    Box::pin((async move || {
+    Box::pin(async {
         proxmox::api::IntoApiOutput::into_api_output(param)
-    })())
+    })
 }
 
 proxmox_api_macro::router! {
