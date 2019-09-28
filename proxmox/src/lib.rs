29893d66ae97dbfdd9cc242a931902885dc4a790
Wolfgang Bumiller <w.bumiller@proxmox.com>
2019-09-28 09:53:40 +0200

    put api macro crate behind 'api-macro' feature
    Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>

--- proxmox/src/lib.rs
+++ proxmox/src/lib.rs
@@ -5,5 +5,6 @@ pub use proxmox_tools as tools;
 // exposed via `proxmox::api`.
 pub mod api {
     pub use proxmox_api::*;
+    #[cfg(feature = "api-macro")]
     pub use proxmox_api_macro::{api, router};
 }
