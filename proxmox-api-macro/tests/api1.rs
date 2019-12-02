7fd69f8b12975248b1b710f5f0b96a46e6a58fca
Wolfgang Bumiller <w.bumiller@proxmox.com>
2019-12-02 11:12:34 +0100

    api-macro: test optional value invocation
    Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>

--- proxmox-api-macro/tests/api1.rs
+++ proxmox-api-macro/tests/api1.rs
@@ -1,7 +1,7 @@
 use proxmox_api_macro::api;
 
 use failure::Error;
-use serde_json::Value;
+use serde_json::{json, Value};
 
 #[api(
     input: {
@@ -105,4 +105,64 @@ pub fn some_call(verbose: bool) -> Result<(), Error> {
 /// Basic function
 pub fn basic_function() -> Result<(), Error> {
     Ok(())
+}
+
+#[api(
+    input: {
+        properties: {
+            verbose: {
+                type: Boolean,
+                optional: true,
+                description: "Verbose output.",
+            },
+        },
+    },
+)]
+/// Optional parameter
+pub fn func_with_option(verbose: Option<bool>) -> Result<(), Error> {
+    let _ = verbose;
+    Ok(())
+}
+
+struct RpcEnv;
+impl proxmox::api::RpcEnvironment for RpcEnv {
+    fn set_result_attrib(&mut self, name: &str, value: Value) {
+        let _ = (name, value);
+        panic!("set_result_attrib called");
+    }
+
+    /// Query additional result data.
+    fn get_result_attrib(&self, name: &str) -> Option<&Value> {
+        let _ = name;
+        panic!("get_result_attrib called");
+    }
+
+    /// The environment type
+    fn env_type(&self) -> proxmox::api::RpcEnvironmentType {
+        panic!("env_type called");
+    }
+
+    /// Set user name
+    fn set_user(&mut self, user: Option<String>) {
+        let _ = user;
+        panic!("set_user called");
+    }
+
+    /// Get user name
+    fn get_user(&self) -> Option<String> {
+        panic!("get_user called");
+    }
+}
+
+#[test]
+fn test_invocations() {
+    let mut env = RpcEnv;
+    api_function_func_with_option(json!({}), &API_METHOD_FUNC_WITH_OPTION, &mut env)
+        .expect("func with option should work");
+    api_function_func_with_option(
+        json!({ "verbose": true }),
+        &API_METHOD_FUNC_WITH_OPTION,
+        &mut env,
+    )
+    .expect("func with option should work");
 }
