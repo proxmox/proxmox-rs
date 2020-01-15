66d117f422e654291fcc20407a567f39c5ad7a93
Wolfgang Bumiller <w.bumiller@proxmox.com>
2020-01-15 13:11:05 +0100

    improve build.sh
    set $CARGO and $RUSTC env vars to also work for users with
    rustup paths in their $PATH.
    
    Replace $(pwd) with $PWD
    
    Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>

--- build.sh
+++ build.sh
@@ -2,12 +2,15 @@
 
 set -e
 
+export CARGO=/usr/bin/cargo
+export RUSTC=/usr/bin/rustc
+
 CRATE=$1
 BUILDCMD=${BUILDCMD:-"dpkg-buildpackage -b -uc -us"}
 
 mkdir -p build
 rm -rf "build/${CRATE}"
 
-debcargo package --config "$(pwd)/${CRATE}/debian/debcargo.toml" --changelog-ready --no-overlay-write-back --directory "$(pwd)/build/${CRATE}" "${CRATE}" "$(dpkg-parsechangelog -l "${CRATE}/debian/changelog" -SVersion | sed -e 's/-.*//')"
+debcargo package --config "$PWD/${CRATE}/debian/debcargo.toml" --changelog-ready --no-overlay-write-back --directory "$PWD/build/${CRATE}" "${CRATE}" "$(dpkg-parsechangelog -l "${CRATE}/debian/changelog" -SVersion | sed -e 's/-.*//')"
 cd "build/${CRATE}"
 ${BUILDCMD}
