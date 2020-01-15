95ab7b68651d4d2dff85f3f4529eda1142ab791e
Wolfgang Bumiller <w.bumiller@proxmox.com>
2020-01-15 16:25:14 +0100

    build.sh: point to system compiler via rust-toolchain
    Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ CRATE=$1
 BUILDCMD=${BUILDCMD:-"dpkg-buildpackage -b -uc -us"}
 
 mkdir -p build
+echo system >build/rust-toolchain
 rm -rf "build/${CRATE}"
 
 debcargo package --config "$PWD/${CRATE}/debian/debcargo.toml" --changelog-ready --no-overlay-write-back --directory "$PWD/build/${CRATE}" "${CRATE}" "$(dpkg-parsechangelog -l "${CRATE}/debian/changelog" -SVersion | sed -e 's/-.*//')"
