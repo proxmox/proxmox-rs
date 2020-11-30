cce052e97f5b4a5d4089dab558adfaeff6fc9b16
Fabian Grünbichler <f.gruenbichler@proxmox.com>
2020-11-30 15:08:05 +0100

    build: track d/control in git
    and check build-dependencies before building..
    
    Signed-off-by: Fabian Grünbichler <f.gruenbichler@proxmox.com>

--- build.sh
+++ build.sh
@@ -12,6 +12,16 @@ mkdir -p build
 echo system >build/rust-toolchain
 rm -rf "build/${CRATE}"
 
+CONTROL="$PWD/${CRATE}/debian/control"
+
+if [ -e "$CONTROL" ]; then
+    # check but only warn, debcargo fails anyway if crates are missing
+    dpkg-checkbuilddeps $PWD/${CRATE}/debian/control || true
+    rm -f "$PWD/${CRATE}/debian/control"
+fi
+
 debcargo package --config "$PWD/${CRATE}/debian/debcargo.toml" --changelog-ready --no-overlay-write-back --directory "$PWD/build/${CRATE}" "${CRATE}" "$(dpkg-parsechangelog -l "${CRATE}/debian/changelog" -SVersion | sed -e 's/-.*//')"
 cd "build/${CRATE}"
 ${BUILDCMD}
+
+cp debian/control "$CONTROL"
