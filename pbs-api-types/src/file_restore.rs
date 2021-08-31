199227bd01a64d2a12328832d771dc2344591da8
Wolfgang Bumiller <w.bumiller@proxmox.com>
2021-08-31 10:45:32 +0200

    move some more API types
    ArchiveEntry -> pbs-datastore
    RestoreDaemonStatus -> pbs-api-types
    
    Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>

---
+++ pbs-api-types/src/file_restore.rs
@@ -0,0 +1,15 @@
+use serde::{Deserialize, Serialize};
+
+use proxmox::api::api;
+
+#[api]
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+/// General status information about a running VM file-restore daemon
+pub struct RestoreDaemonStatus {
+    /// VM uptime in seconds
+    pub uptime: i64,
+    /// time left until auto-shutdown, keep in mind that this is useless when 'keep-timeout' is
+    /// not set, as then the status call will have reset the timer before returning the value
+    pub timeout: i64,
+}
