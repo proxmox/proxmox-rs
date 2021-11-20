6c4982bd7cd964087f9fc304a04133e8ec37c38f
Dietmar Maurer <dietmar@proxmox.com>
2021-11-20 16:15:05 +0100

    proxmox-async: remove duplicate src/stream/wrapped_reader_stream.rs
    Signed-off-by: Dietmar Maurer <dietmar@proxmox.com>

--- proxmox-async/src/stream/mod.rs
+++ proxmox-async/src/stream/mod.rs
@@ -5,6 +5,3 @@ pub use async_channel_writer::AsyncChannelWriter;
 
 mod async_reader_stream;
 pub use async_reader_stream::AsyncReaderStream;
-
-mod wrapped_reader_stream;
-pub use wrapped_reader_stream::WrappedReaderStream;
