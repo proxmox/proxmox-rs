7b7247fa803ba4170c864391300a68d3947b9a0c
Dietmar Maurer <dietmar@proxmox.com>
2021-11-20 16:38:36 +0100

    proxmox-async: move TokioWriterAdapter to blocking
    Signed-off-by: Dietmar Maurer <dietmar@proxmox.com>

--- proxmox-async/src/blocking/mod.rs
+++ proxmox-async/src/blocking/mod.rs
@@ -4,5 +4,8 @@
 mod std_channel_stream;
 pub use std_channel_stream::StdChannelStream;
 
+mod tokio_writer_adapter;
+pub use tokio_writer_adapter::TokioWriterAdapter;
+
 mod wrapped_reader_stream;
 pub use wrapped_reader_stream::WrappedReaderStream;
--- proxmox-async/src/lib.rs
+++ proxmox-async/src/lib.rs
@@ -3,5 +3,4 @@ pub mod broadcast_future;
 pub mod compression;
 pub mod runtime;
 pub mod stream;
-pub mod tokio_writer_adapter;
 pub mod zip;
