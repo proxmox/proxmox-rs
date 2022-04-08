2c9272945ed2b68dec494abf2d00f2abcda4a00f
Dominik Csapak <d.csapak@proxmox.com>
2022-04-08 11:56:01 +0200

    promxox-router: add SerializableReturn Trait
    this will be useful as a generic return type for api calls which
    must implement Serialize.
    
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>
    Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>

--- proxmox-router/src/lib.rs
+++ proxmox-router/src/lib.rs
@@ -12,6 +12,7 @@ pub mod error;
 mod permission;
 mod router;
 mod rpc_environment;
+mod serializable_return;
 
 #[doc(inline)]
 pub use error::HttpError;
@@ -19,6 +20,7 @@ pub use error::HttpError;
 pub use permission::*;
 pub use router::*;
 pub use rpc_environment::{RpcEnvironment, RpcEnvironmentType};
+pub use serializable_return::SerializableReturn;
 
 // make list_subdirs_api_method! work without an explicit proxmox-schema dependency:
 #[doc(hidden)]
---
+++ proxmox-router/src/serializable_return.rs
@@ -0,0 +1,62 @@
+use serde::Serializer;
+use serde_json::Value;
+
+/// This defines a *fixed* serializer (iow. also where/how to write out the data).
+///
+/// (Note that `serde::Serializer` is implemented for `__&mut__ serde_json::Serializer`.
+type SenderSerializer<'a> = &'a mut serde_json::Serializer<
+    &'a mut std::io::BufWriter<proxmox_async::blocking::SenderWriter>,
+>;
+
+/// This is an object-safe trait which requires the ability to serialize into particular
+/// Serializer instances.
+pub trait SerializableReturn {
+    /// Serializes self into a [`proxmox_async::blocking::SenderWriter`] wrapped
+    /// into a [`std::io::BufWriter`]
+    ///
+    /// If `value` is an Object/Map, serializes that first and puts the value of
+    /// `self` into the `data` property.
+    fn sender_serialize(
+        &self,
+        serializer: SenderSerializer,
+        value: Value,
+    ) -> Result<
+        <SenderSerializer as serde::Serializer>::Ok,
+        <SenderSerializer as serde::Serializer>::Error,
+    >;
+
+    /// Returns a value again from self
+    fn to_value(&self) -> Result<Value, serde_json::error::Error>;
+}
+
+impl<T> SerializableReturn for T
+where
+    T: serde::Serialize,
+{
+    fn sender_serialize(
+        &self,
+        serializer: SenderSerializer,
+        value: Value,
+    ) -> Result<
+        <SenderSerializer as serde::Serializer>::Ok,
+        <SenderSerializer as serde::Serializer>::Error,
+    > {
+        use serde::ser::SerializeMap;
+        if let Some(original) = value.as_object() {
+            let mut map = serializer.serialize_map(None)?;
+            for (k, v) in original {
+                map.serialize_entry(k, v)?;
+            }
+
+            map.serialize_key("data")?;
+            map.serialize_value(&self)?;
+            map.end()
+        } else {
+            self.serialize(serializer)
+        }
+    }
+
+    fn to_value(&self) -> Result<Value, serde_json::error::Error> {
+        serde_json::to_value(self)
+    }
+}
