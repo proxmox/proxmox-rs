proxmox/proxmox-rs

commit fdcb2694b44e5e6121d4da13ee0b748cff729e17

datastore status: factor out api type DataStoreStatusListItem
And use the rust type instead of json::Value.

pbs-api-types/src/datastore.rs
@@ -633,6 +633,48 @@ pub struct DataStoreStatus {
     pub counts: Option<Counts>,
 }
 
+#[api(
+    properties: {
+        store: {
+            schema: DATASTORE_SCHEMA,
+        },
+        history: {
+            type: Array,
+            optional: true,
+            items: {
+                type: Number,
+                description: "The usage of a time in the past. Either null or between 0.0 and 1.0.",
+            }
+        },
+     },
+)]
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all="kebab-case")]
+/// Status of a Datastore
+pub struct DataStoreStatusListItem {
+    pub store: String,
+    /// The Size of the underlying storage in bytes.
+    pub total: u64,
+    /// The used bytes of the underlying storage.
+    pub used: u64,
+    /// The available bytes of the underlying storage.
+    pub avail: u64,
+    /// A list of usages of the past (last Month).
+    pub history: Option<Vec<Option<f64>>>,
+    /// History start time (epoch)
+    pub history_start: Option<u64>,
+    /// History resolution (seconds)
+    pub history_delta: Option<u64>,
+    /// Estimation of the UNIX epoch when the storage will be full.
+    /// This is calculated via a simple Linear Regression (Least
+    /// Squares) of RRD data of the last Month. Missing if there are
+    /// not enough data points yet. If the estimate lies in the past,
+    /// the usage is decreasing.
+    pub estimated_full_date: Option<i64>,
+    /// An error description, for example, when the datastore could not be looked up
+    pub error: Option<String>,
+}
+
 pub const ADMIN_DATASTORE_LIST_SNAPSHOTS_RETURN_TYPE: ReturnType = ReturnType {
     optional: false,
     schema: &ArraySchema::new(

commit c2049bce7fd082148751cb3324ef5b69026cd2bd

api: status: return gc-status again
Returning the GC status was dropped by mistake in commit fdcb2694
("datastore status: factor out api type DataStoreStatusListItem")

As this is considered a breaking change which we also felt, due to
the gc-status being used in the web interface for the datastore
overview list (not the dashboard), re add it.

Fixes: fdcb2694
Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>
 [ T: add reference to breaking commit, reword message ]
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

pbs-api-types/src/datastore.rs
@@ -975,6 +975,9 @@ pub struct DataStoreStatusListItem {
     /// An error description, for example, when the datastore could not be looked up
     #[serde(skip_serializing_if = "Option::is_none")]
     pub error: Option<String>,
+    /// Status of last GC
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub gc_status: Option<GarbageCollectionStatus>,
 }
 
 pub const ADMIN_DATASTORE_LIST_SNAPSHOTS_RETURN_TYPE: ReturnType = ReturnType {