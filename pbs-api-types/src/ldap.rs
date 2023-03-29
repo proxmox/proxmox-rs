proxmox/proxmox-rs

commit 3aba0d9aa64e821aa6fa9e77d037544c64dc74b4

api-types: ldap: add verification regex for LDAP DNs
Regex was taken from the LDAP implementation in PVE.

Signed-off-by: Lukas Wagner <l.wagner@proxmox.com>

pbs-api-types/src/ldap.rs
@@ -1,6 +1,8 @@
 use serde::{Deserialize, Serialize};
 
-use proxmox_schema::{api, ApiStringFormat, ApiType, ArraySchema, Schema, StringSchema, Updater};
+use proxmox_schema::{
+    api, const_regex, ApiStringFormat, ApiType, ArraySchema, Schema, StringSchema, Updater,
+};
 
 use super::{REALM_ID_SCHEMA, SINGLE_LINE_COMMENT_SCHEMA};
 
@@ -45,6 +47,13 @@ pub enum LdapMode {
             optional: true,
             schema: USER_CLASSES_SCHEMA,
         },
+        "base-dn" : {
+            schema: LDAP_DOMAIN_SCHEMA,
+        },
+        "bind-dn" : {
+            schema: LDAP_DOMAIN_SCHEMA,
+            optional: true,
+        }
     },
 )]
 #[derive(Serialize, Deserialize, Updater, Clone)]
@@ -132,6 +141,28 @@ pub enum RemoveVanished {
     /// Remove vanished properties from users (e.g. email)
     Properties,
 }
+
+macro_rules! DOMAIN_PART_REGEX {
+    () => {
+        r#"("[^"]+"|[^ ,+"/<>;=#][^,+"/<>;=]*[^ ,+"/<>;=]|[^ ,+"/<>;=#])"#
+    };
+}
+
+const_regex! {
+    pub LDAP_DOMAIN_REGEX = concat!(
+        r#"\w+="#,
+        DOMAIN_PART_REGEX!(),
+        r#"(,\s*\w+="#,
+        DOMAIN_PART_REGEX!(),
+        ")*"
+    );
+}
+
+pub const LDAP_DOMAIN_FORMAT: ApiStringFormat = ApiStringFormat::Pattern(&LDAP_DOMAIN_REGEX);
+
+pub const LDAP_DOMAIN_SCHEMA: Schema = StringSchema::new("LDAP Domain")
+    .format(&LDAP_DOMAIN_FORMAT)
+    .schema();
 
 pub const SYNC_DEFAULTS_STRING_SCHEMA: Schema = StringSchema::new("sync defaults options")
     .format(&ApiStringFormat::PropertyString(

commit 92cca750d9a98da19d2c5c0dcacae9c72e579706

api-types: ldap: properly anchor DN regex
Otherwise, a substring match is enough to fulfill the constraint.

Fixes: 3aba0d9a ("api-types: ldap: add verification regex for LDAP DNs")
Reported-by: Friedrich Weber <f.weber@proxmox.com>
Signed-off-by: Lukas Wagner <l.wagner@proxmox.com>

pbs-api-types/src/ldap.rs
@@ -150,11 +150,11 @@ macro_rules! DOMAIN_PART_REGEX {
 
 const_regex! {
     pub LDAP_DOMAIN_REGEX = concat!(
-        r#"\w+="#,
+        r#"^\w+="#,
         DOMAIN_PART_REGEX!(),
         r#"(,\s*\w+="#,
         DOMAIN_PART_REGEX!(),
-        ")*"
+        ")*$"
     );
 }
 