54108f269ab083d848a22d8402f9cb3ff4d93f9a
Wolfgang Bumiller <w.bumiller@proxmox.com>
2023-08-03 08:32:00 +0200

    export hyper-client types
    Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>

--- pve-api-types/src/client/mod.rs
+++ pve-api-types/src/client/mod.rs
@@ -7,6 +7,8 @@ pub use code::*;
 
 #[cfg(feature = "hyper-client")]
 mod hyper_client;
+#[cfg(feature = "hyper-client")]
+pub use hyper_client::{BadFingerprint, HyperClient, Options};
 
 /// Add an optional string parameter to the query, and if it was added, change `separator` to `&`.
 pub fn add_query_arg<T>(query: &mut String, separator: &mut char, name: &str, value: &Option<T>)
