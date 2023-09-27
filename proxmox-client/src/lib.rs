proxmox/proxmox-rs

commit 271a55f187cf862c66388b641cc313244c33db87

client: remove option from inner RawApiResponse
when using the client for an api call that does not return any data
(it returns '{"data":null}'), we would always get an error 'api returned
no data'. The message is technically correct, but it should not be an
error when we expect no data (e.g. most of our CRUD PUT/POST calls)

instead of having the Option<T> in the RawApiResponse type itself, move
it into to the 'nodata' function intended for api calls where we don't
expect any data.

Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

proxmox-client/src/lib.rs
@@ -106,8 +106,8 @@ impl HttpApiResponse {
 
     /// Expect that the API call did *not* return any data in the `data` field.
     pub fn nodata(self) -> Result<(), Error> {
-        let response = serde_json::from_slice::<RawApiResponse<()>>(&self.body)
-            .map_err(|err| Error::bad_api("failed to parse api response", err))?;
+        let response = serde_json::from_slice::<RawApiResponse<Option<()>>>(&self.body)
+            .map_err(|err| Error::bad_api("unexpected api response", err))?;
 
         if response.data.is_some() {
             Err(Error::UnexpectedData)
@@ -131,7 +131,7 @@ struct RawApiResponse<T> {
     message: Option<String>,
     #[serde(default, deserialize_with = "proxmox_login::parse::deserialize_bool")]
     success: Option<bool>,
-    data: Option<T>,
+    data: T,
 
     #[serde(default)]
     errors: HashMap<String, String>,
@@ -164,9 +164,7 @@ impl<T> RawApiResponse<T> {
         let this = self.check_success()?;
 
         Ok(ApiResponseData {
-            data: this
-                .data
-                .ok_or_else(|| Error::BadApi("api returned no data".to_string(), None))?,
+            data: this.data,
             attribs: this.attribs,
         })
     }

commit 9fa41851961d0e2eb8b25c495221e82f2bd49c8b

client: fix optional data for errors
previously we changed the internal type of the 'data' property
from Option<T> to T in the assumption the api always returns
'data:null'.

this is actually only the case when the api call succeeds. in an error
case there is no data property at all.

to fix this issue while behaving the same for 'data:null' we have to
revert to Option<T> for RawApiResponse but instead of always throwing an
error for 'data:null' in 'check' we now try there to deserialize from
Value::Null for T if there was no data. This will succeed for the Type
'()' which was the motivation for the original change.

The only downside is that the RawApiResponse now has a trait bound that
T is deserializeable, but was a requirement for using it anyway
(as there was no other way of constructing it)

Fixes: 271a55f ("client: remove option from inner RawApiResponse")
Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

proxmox-client/src/lib.rs
@@ -106,7 +106,7 @@ impl HttpApiResponse {
 
     /// Expect that the API call did *not* return any data in the `data` field.
     pub fn nodata(self) -> Result<(), Error> {
-        let response = serde_json::from_slice::<RawApiResponse<Option<()>>>(&self.body)
+        let response = serde_json::from_slice::<RawApiResponse<()>>(&self.body)
             .map_err(|err| Error::bad_api("unexpected api response", err))?;
 
         if response.data.is_some() {
@@ -131,7 +131,7 @@ struct RawApiResponse<T> {
     message: Option<String>,
     #[serde(default, deserialize_with = "proxmox_login::parse::deserialize_bool")]
     success: Option<bool>,
-    data: T,
+    data: Option<T>,
 
     #[serde(default)]
     errors: HashMap<String, String>,
@@ -140,7 +140,10 @@ struct RawApiResponse<T> {
     attribs: HashMap<String, Value>,
 }
 
-impl<T> RawApiResponse<T> {
+impl<T> RawApiResponse<T>
+where
+    T: for<'de> Deserialize<'de>,
+{
     fn check_success(mut self) -> Result<Self, Error> {
         if self.success == Some(true) {
             return Ok(self);
@@ -163,8 +166,16 @@ impl<T> RawApiResponse<T> {
     fn check(self) -> Result<ApiResponseData<T>, Error> {
         let this = self.check_success()?;
 
+        // RawApiResponse has no data, but this also happens for Value::Null, and T
+        // might be deserializeable from that, so try here again
+        let data = match this.data {
+            Some(data) => data,
+            None => serde_json::from_value(Value::Null)
+                .map_err(|_| Error::BadApi("api returned no data".to_string(), None))?,
+        };
+
         Ok(ApiResponseData {
-            data: this.data,
+            data,
             attribs: this.attribs,
         })
     }