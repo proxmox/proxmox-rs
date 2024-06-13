59f1bdbe85fa3c7c678f57910a3509d69515e877
Wolfgang Bumiller <w.bumiller@proxmox.com>
2024-06-13 12:44:54 +0200

    router: cli: store extra CLI args by type
    The CLI environment can now contain ApiType structs which can be
    accessed by their type.
    The TypeId is used since the options inside must be unique anyway and
    we can't have the same type specified multiple times. It also makes
    for a somewhat convenient interface:
    
        env.take_global_option::<ConnectInfo>()
    
    where ConnectInfo is a struct containing the server, user, port, ...
    since these will not be passed as *parameters* to the API functions.
    
    Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>

--- proxmox-router/src/cli/environment.rs
+++ proxmox-router/src/cli/environment.rs
@@ -1,4 +1,9 @@
+use std::any::{Any, TypeId};
+use std::collections::HashMap;
+
 use serde_json::Value;
+
+use proxmox_schema::ApiType;
 
 use crate::{RpcEnvironment, RpcEnvironmentType};
 
@@ -7,11 +12,52 @@ use crate::{RpcEnvironment, RpcEnvironmentType};
 pub struct CliEnvironment {
     result_attributes: Value,
     auth_id: Option<String>,
+    pub(crate) global_options: HashMap<TypeId, Box<dyn Any + Send + Sync + 'static>>,
 }
 
 impl CliEnvironment {
     pub fn new() -> Self {
         Default::default()
+    }
+
+    /// Get a specific command line argument type.
+    pub fn global_option<T>(&self) -> Option<&T>
+    where
+        T: ApiType + Any + Send + Sync + 'static,
+    {
+        Some(
+            self.global_options
+                .get(&TypeId::of::<T>())?
+                .downcast_ref::<T>()
+                .unwrap(), // the map must only store correctly typed items!
+        )
+    }
+
+    /// Get a mutable reference to a specific command line argument type.
+    pub fn global_option_mut<T>(&mut self) -> Option<&T>
+    where
+        T: ApiType + Any + Send + Sync + 'static,
+    {
+        Some(
+            self.global_options
+                .get_mut(&TypeId::of::<T>())?
+                .downcast_mut::<T>()
+                .unwrap(), // the map must only store correctly typed items!
+        )
+    }
+
+    /// Take a command line argument struct out of the argument list.
+    pub fn take_global_option<T>(&mut self) -> Option<T>
+    where
+        T: ApiType + Any + Send + Sync + 'static,
+    {
+        Some(
+            *self
+                .global_options
+                .remove(&TypeId::of::<T>())?
+                .downcast::<T>()
+                .unwrap(), // the map must only store correctly typed items!
+        )
     }
 }
 
