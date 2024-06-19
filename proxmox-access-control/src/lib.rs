46d8423d72cb1a56a3dfab64dcc517064c513534
Shannon Sterz <s.sterz@proxmox.com>
2024-06-19 11:54:18 +0200

    access-control: split crate in `default` and `impl` features
    this way the types defined in this crate can be re-used in places
    without necessarily having to use the ACL, token shadow and
    (cached) user config implementations.
    
    Signed-off-by: Shannon Sterz <s.sterz@proxmox.com>

--- proxmox-access-control/src/lib.rs
+++ proxmox-access-control/src/lib.rs
@@ -1,8 +1,18 @@
-pub mod acl;
-pub mod init;
-pub mod token_shadow;
 pub mod types;
+
+#[cfg(feature = "impl")]
+pub mod acl;
+
+#[cfg(feature = "impl")]
+pub mod init;
+
+#[cfg(feature = "impl")]
+pub mod token_shadow;
+
+#[cfg(feature = "impl")]
 pub mod user;
 
+#[cfg(feature = "impl")]
 mod cached_user_info;
+#[cfg(feature = "impl")]
 pub use cached_user_info::CachedUserInfo;
