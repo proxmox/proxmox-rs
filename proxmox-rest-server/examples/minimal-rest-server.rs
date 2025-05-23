proxmox/proxmox-rs

commit 48fc0a2a07966fdefc3c9f7d75363c62389c3d39

http: use Watcher of GracefulShutdown to avoid races
Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>

proxmox-rest-server/examples/minimal-rest-server.rs
@@ -191,7 +191,7 @@ const SUBDIRS: SubdirMap = &[
 const ROUTER: Router = Router::new()
     .get(&list_subdirs_api_method!(SUBDIRS))
     .subdirs(SUBDIRS);
-
+                                e
 async fn run() -> Result<(), Error> {
     // we first have to configure the api environment (basedir etc.)
 
@@ -214,15 +214,15 @@ async fn run() -> Result<(), Error> {
         ([127, 0, 0, 1], 65000).into(),
         move |listener: TcpListener| {
             Ok(async move {
-                let graceful = Arc::new(GracefulShutdown::new());
+                let graceful = GracefulShutdown::new();
                 loop {
-                    let graceful2 = Arc::clone(&graceful);
                     tokio::select! {
                         incoming = listener.accept() => {
                             log::info!("accepted new connection!");
                             let (conn, _) = incoming?;
                             let api_service = rest_server.api_service(&conn)?;
-                            tokio::spawn(async move { let res = api_service.serve(conn, Some(graceful2)).await; log::info!("connection finished: {res:?}") });
+                            let watcher = graceful.watcher();
+                            tokio::spawn(async move { let res = api_service.serve(conn, Some(watcher)).await; log::info!("connection finished: {res:?}") });
                         },
                         _shutdown = proxmox_daemon::shutdown_future() => {
                             log::info!("shutdown future triggered!");
@@ -230,11 +230,8 @@ async fn run() -> Result<(), Error> {
                         }
                     }
                 }
-                log::info!("count {}", Arc::strong_count(&graceful));
-                if let Some(shutdown) = Arc::into_inner(graceful) {
-                    log::info!("shutting down..");
-                    shutdown.shutdown().await
-                }
+                log::info!("shutting down..");
+                shutdown.shutdown().await
                 Ok(())
             })
         },

proxmox-rest-server/src/rest.rs
@@ -17,7 +17,7 @@ use hyper::http::request::Parts;
 use hyper::{Request, Response, StatusCode};
 use hyper_util::rt::{TokioExecutor, TokioIo};
 use hyper_util::server::conn;
-use hyper_util::server::graceful::GracefulShutdown;
+use hyper_util::server::graceful;
 use hyper_util::service::TowerToHyperService;
 use regex::Regex;
 use serde_json::Value;
@@ -119,7 +119,7 @@ impl RedirectService {
     pub async fn serve<S>(
         self,
         conn: S,
-        mut graceful: Option<Arc<GracefulShutdown>>,
+        mut graceful: Option<graceful::Watcher>,
     ) -> Result<(), Error>
     where
         S: AsyncRead + AsyncWrite + Unpin + Send + 'static,
@@ -130,7 +130,6 @@ impl RedirectService {
         let api_conn = api_conn.serve_connection_with_upgrades(io, api_service);
         if let Some(graceful) = graceful.take() {
             let api_conn = graceful.watch(api_conn);
-            drop(graceful);
             api_conn.await
         } else {
             api_conn.await
@@ -240,7 +239,7 @@ impl ApiService {
     pub async fn serve<S>(
         self,
         conn: S,
-        mut graceful: Option<Arc<GracefulShutdown>>,
+        mut graceful: Option<graceful::Watcher>,
     ) -> Result<(), Error>
     where
         S: AsyncRead + AsyncWrite + Unpin + Send + 'static,
@@ -251,7 +250,6 @@ impl ApiService {
         let api_conn = api_conn.serve_connection_with_upgrades(io, api_service);
         if let Some(graceful) = graceful.take() {
             let api_conn = graceful.watch(api_conn);
-            drop(graceful);
             api_conn.await
         } else {
             api_conn.await

commit 01894fd657afaec273d248a200204732d331c6d3

rest-server: make minimal example compile again
This was quite definitively applied due to debcargo not running the
tests with all features enabled.
While that indeed cannot be done in general due to potential
conflicting features where that itself will result in compiler errors,
it could be enabled as opt-out, or with some settings for feature
combinations to skip, but I digress.

Fixes: 48fc0a2a ("http: use Watcher of GracefulShutdown to avoid races")
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

proxmox-rest-server/examples/minimal-rest-server.rs
@@ -1,15 +1,16 @@
 use std::collections::HashMap;
 use std::future::Future;
 use std::pin::Pin;
-use std::sync::{Arc, LazyLock, Mutex};
+use std::sync::{LazyLock, Mutex};
 
 use anyhow::{bail, format_err, Error};
 use futures::future;
 use http::request::Parts;
 use http::HeaderMap;
 use hyper::{Method, Response};
-
 use hyper_util::server::graceful::GracefulShutdown;
+use tokio::net::TcpListener;
+
 use proxmox_http::Body;
 use proxmox_log::LevelFilter;
 use proxmox_router::{
@@ -18,7 +19,6 @@ use proxmox_router::{
 use proxmox_schema::api;
 
 use proxmox_rest_server::{ApiConfig, AuthError, RestEnvironment, RestServer};
-use tokio::net::TcpListener;
 
 // Create a Dummy User information system
 struct DummyUserInfo;
@@ -191,7 +191,7 @@ const SUBDIRS: SubdirMap = &[
 const ROUTER: Router = Router::new()
     .get(&list_subdirs_api_method!(SUBDIRS))
     .subdirs(SUBDIRS);
-                                e
+
 async fn run() -> Result<(), Error> {
     // we first have to configure the api environment (basedir etc.)
 
@@ -231,7 +231,7 @@ async fn run() -> Result<(), Error> {
                     }
                 }
                 log::info!("shutting down..");
-                shutdown.shutdown().await
+                graceful.shutdown().await;
                 Ok(())
             })
         },