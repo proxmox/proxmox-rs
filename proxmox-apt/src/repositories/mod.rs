proxmox/proxmox-rs

commit 48ab96212e4ea80b493dd2e3900e935c85406784

apt: output standard repos as in deb822 sources format for Trixie and newer
This was not tested in practice and might need follow ups, I enable it
nonetheless as it should better ensure that we do not forget and thus
avoid a nosiy warning by apt.

Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

proxmox-apt/src/repositories/standard.rs
@@ -151,6 +151,12 @@ impl APTRepositoryHandleImpl for APTRepositoryHandle {
     fn to_repository(self, product: &str, suite: &str) -> APTRepository {
         let (package_type, uris, component) = self.info(product);
 
+        use crate::repositories::DebianCodename;
+        let file_type = match DebianCodename::try_from(suite) {
+            Ok(codename) if codename >= DebianCodename::Trixie => APTRepositoryFileType::Sources,
+            _ => APTRepositoryFileType::List,
+        };
+
         APTRepository {
             types: vec![package_type],
             uris: vec![uris.into_iter().next().unwrap()],
@@ -158,7 +164,7 @@ impl APTRepositoryHandleImpl for APTRepositoryHandle {
             components: vec![component],
             options: vec![],
             comment: String::new(),
-            file_type: APTRepositoryFileType::List,
+            file_type,
             enabled: true,
         }
     }

commit 2fa47f3038a4f421189e607ab1072fa71189b7d2

apt: standard repos: use correct path for deb822 sources
We default to deb822 sources for Debian Trixie–and newer–based
releases, but forgot to adapt the path used for adding a repo
depending on which type is used.

Fixes: 48ab9621 ("apt: output standard repos as in deb822 sources format for Trixie and newer")
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

proxmox-apt/src/repositories/mod.rs
@@ -71,8 +71,10 @@ pub fn get_standard_repository(
     product: &str,
     suite: DebianCodename,
 ) -> (APTRepository, String) {
-    let repo = handle.to_repository(product, &suite.to_string());
-    let path = handle.path(product);
+    let suite = suite.to_string();
+
+    let repo = handle.to_repository(product, &suite);
+    let path = handle.path(product, &suite);
 
     (repo, path)
 }

proxmox-apt/src/repositories/standard.rs
@@ -3,6 +3,8 @@ use proxmox_apt_api_types::{
     APTStandardRepository,
 };
 
+use crate::repositories::DebianCodename;
+
 pub trait APTStandardRepositoryImpl {
     fn from_handle(handle: APTRepositoryHandle) -> APTStandardRepository;
 }
@@ -24,7 +26,7 @@ pub trait APTRepositoryHandleImpl {
     /// Get the display name of the repository.
     fn name(self) -> String;
     /// Get the standard file path for the repository referenced by the handle.
-    fn path(self, product: &str) -> String;
+    fn path(self, product: &str, suite: &str) -> String;
     /// Get package type, possible URIs and the component associated with the handle.
     ///
     /// The first URI is the preferred one.
@@ -79,16 +81,33 @@ impl APTRepositoryHandleImpl for APTRepositoryHandle {
         .to_string()
     }
 
-    fn path(self, product: &str) -> String {
-        match self {
-            APTRepositoryHandle::Enterprise => {
-                format!("/etc/apt/sources.list.d/{product}-enterprise.list")
-            }
-            APTRepositoryHandle::NoSubscription => "/etc/apt/sources.list".to_string(),
-            APTRepositoryHandle::Test => "/etc/apt/sources.list".to_string(),
-            APTRepositoryHandle::CephSquidEnterprise
-            | APTRepositoryHandle::CephSquidNoSubscription
-            | APTRepositoryHandle::CephSquidTest => "/etc/apt/sources.list.d/ceph.list".to_string(),
+    fn path(self, product: &str, suite: &str) -> String {
+        match DebianCodename::try_from(suite) {
+            Ok(codename) if codename >= DebianCodename::Trixie => match self {
+                APTRepositoryHandle::Enterprise => {
+                    format!("/etc/apt/sources.list.d/{product}-enterprise.sources")
+                }
+                APTRepositoryHandle::NoSubscription | APTRepositoryHandle::Test => {
+                    "/etc/apt/sources.list.d/proxmox.sources".to_string()
+                }
+                APTRepositoryHandle::CephSquidEnterprise
+                | APTRepositoryHandle::CephSquidNoSubscription
+                | APTRepositoryHandle::CephSquidTest => {
+                    "/etc/apt/sources.list.d/ceph.sources".to_string()
+                }
+            },
+            _ => match self {
+                APTRepositoryHandle::Enterprise => {
+                    format!("/etc/apt/sources.list.d/{product}-enterprise.list")
+                }
+                APTRepositoryHandle::NoSubscription => "/etc/apt/sources.list".to_string(),
+                APTRepositoryHandle::Test => "/etc/apt/sources.list".to_string(),
+                APTRepositoryHandle::CephSquidEnterprise
+                | APTRepositoryHandle::CephSquidNoSubscription
+                | APTRepositoryHandle::CephSquidTest => {
+                    "/etc/apt/sources.list.d/ceph.list".to_string()
+                }
+            },
         }
     }
 
@@ -148,7 +167,6 @@ impl APTRepositoryHandleImpl for APTRepositoryHandle {
     fn to_repository(self, product: &str, suite: &str) -> APTRepository {
         let (package_type, uris, component) = self.info(product);
 
-        use crate::repositories::DebianCodename;
         let file_type = match DebianCodename::try_from(suite) {
             Ok(codename) if codename >= DebianCodename::Trixie => APTRepositoryFileType::Sources,
             _ => APTRepositoryFileType::List,