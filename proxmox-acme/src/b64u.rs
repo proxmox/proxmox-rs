74f1b8cf989d5fe5ffd1bcf986496e5ab4df6ee7
Wolfgang Bumiller <w.bumiller@proxmox.com>
2025-07-22 09:11:45 +0200

    acme: turn off base64 padding
    When switching to proxmox-base64 I accidentally used the padded
    encoding function which causes letsencrypt to refuse the requests.
    
    Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>

--- proxmox-acme/src/b64u.rs
+++ proxmox-acme/src/b64u.rs
@@ -1,2 +1,3 @@
-pub use proxmox_base64::url::as_base64 as bytes;
-pub use proxmox_base64::url::{decode, encode};
+pub use proxmox_base64::url::as_base64_no_pad_indifferent as bytes;
+pub use proxmox_base64::url::decode_no_pad as decode;
+pub use proxmox_base64::url::encode_no_pad as encode;
