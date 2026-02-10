proxmox/proxmox-rs

commit 470ee44b20406530645644bc3b2e81e2b4171d9f

rest server: unify returned error message for auth failure
the details are logged in the backend anyway, but do not need to be
returned to the client.

Signed-off-by: Fabian Grünbichler <f.gruenbichler@proxmox.com>
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

proxmox-rest-server/src/rest.rs
@@ -1146,7 +1146,7 @@ impl Formatted {
                     // always delay unauthorized calls by 3 seconds (from start of request)
                     let err = http_err!(UNAUTHORIZED, "authentication failed - {}", err);
                     tokio::time::sleep_until(Instant::from_std(delay_unauth_time())).await;
-                    return Err(err);
+                    return Err(format_err!("authentication failed"));
                 }
             }
         } else {
@@ -1268,7 +1268,7 @@ impl Unformatted {
                     // always delay unauthorized calls by 3 seconds (from start of request)
                     let err = http_err!(UNAUTHORIZED, "authentication failed - {}", err);
                     tokio::time::sleep_until(Instant::from_std(delay_unauth_time())).await;
-                    return Err(err);
+                    return Err(format_err!("authentication failed"));
                 }
             }
         } else {

commit 26b55800ac2caa5b4ec926725b098f5fdfda561b

rest-server: correctly return 401 unauthorized over generic bad request again
We recently made the error message generic, but when doing so we
mistakenly change the returned error from a HTTP specific 401
UNAUTHORIZED error to a generic anyhow one, which the call site mapped
to BAD REQUEST one, and that in turn can lead to backend or frontend
not clearing some cookies or doing other code that is relevant for
showing a proper login mask again; i.e., nothing security critical as
the request will be aborted in any case and the user is still not
authenticated, but bad UX in any case.

Fixes: 470ee44b ("rest server: unify returned error message for auth failure")
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

proxmox-rest-server/src/rest.rs
@@ -1144,9 +1144,8 @@ impl Formatted {
                     rpcenv.log_failed_auth(None, &err.to_string());
 
                     // always delay unauthorized calls by 3 seconds (from start of request)
-                    let err = http_err!(UNAUTHORIZED, "authentication failed - {}", err);
                     tokio::time::sleep_until(Instant::from_std(delay_unauth_time())).await;
-                    return Err(format_err!("authentication failed"));
+                    return Err(http_err!(UNAUTHORIZED, "authentication failed"));
                 }
             }
         } else {
@@ -1266,9 +1265,8 @@ impl Unformatted {
                     rpcenv.log_failed_auth(None, &err.to_string());
 
                     // always delay unauthorized calls by 3 seconds (from start of request)
-                    let err = http_err!(UNAUTHORIZED, "authentication failed - {}", err);
                     tokio::time::sleep_until(Instant::from_std(delay_unauth_time())).await;
-                    return Err(format_err!("authentication failed"));
+                    return Err(http_err!(UNAUTHORIZED, "authentication failed"));
                 }
             }
         } else {