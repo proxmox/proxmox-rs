e3e6fdd1ffaae40f223f26c186280a3fbbe0733b
Christoph Heiss <c.heiss@proxmox.com>
2026-04-30 14:46:30 +0200

    api-macro: allow $ in identifier name
    This allows dollar-sign in renamed field names for API types.
    
    Reviewed-by: Lukas Wagner <l.wagner@proxmox.com>
    Signed-off-by: Christoph Heiss <c.heiss@proxmox.com>
    Tested-by: Lukas Wagner <l.wagner@proxmox.com>
    Link: https://lore.proxmox.com/20260430124712.1614305-2-c.heiss@proxmox.com
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- proxmox-api-macro/src/util.rs
+++ proxmox-api-macro/src/util.rs
@@ -30,7 +30,7 @@ pub struct FieldName {
 
 impl FieldName {
     pub fn new(name: String, span: Span) -> Self {
-        let mut ident_str = name.replace(['-', '.', '+'].as_ref(), "_");
+        let mut ident_str = name.replace(['-', '.', '+', '$'].as_ref(), "_");
 
         if ident_str.chars().next().unwrap().is_numeric() {
             ident_str.insert(0, '_');
